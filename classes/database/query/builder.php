7613d4c49eeacceb93644434fff8ebfcd0cf46b7
Freshinc <oliver.morgan@freshinc.co.uk>
2009-11-27 23:17:04 +0000

    Added the compile column logic in the query builder.

--- classes/database/query/builder.php
+++ classes/database/query/builder.php
@@ -15,9 +15,15 @@ abstract class Database_Query_Builder extends Kohana_Database_Query_Builder {
 	 * @param   object   The column object.
 	 * @return  strin	The SQL.
 	 */
-	public static function compile_column( Database_Table_Column $column)
+	public static function compile_column(Database_Table_Column $column)
 	{
-		
+		$db = $column->table->database;
+
+		return implode(' ', array(
+			$db->quote_identifier($column->name),
+			$column->compile_datatype(),
+			$column->compile_constraints()
+		));
 	}
 
 } // End Database_Query_Builder
